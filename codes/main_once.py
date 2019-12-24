01578fbe26281edf6caaeabd944d3dcbebf782f5
philip <philip.sss@mail.bnu.edu>
2019-12-24 15:35:24 +0800

    300 iteration save

--- codes/main_once.py
+++ codes/main_once.py
@@ -31,19 +31,18 @@ def Optimize(args, net_path, eval_step=True) :
         opt.step()
 
         tte = time.time()
-        print("Loss={:.2f}, Opt_Time={:.2f}s".format(loss.item(), tte-tts)) 
+        print("Iter={} Loss={:.2f}, Opt_Time={:.2f}s".format(i, loss.item(), tte-tts)) 
 
     SeedSize_Inf = {}
     idx = T.argsort(S, descending=True)
     Seed_Prepar = find_top_K(G, idx, 51, overlap=args.OverLap, uncover_rate=args.OverLapRate)
-    #Seed_Prepar = Greedy_K(IC, device, idx, 51)
     print("Eval Start ...... ")
-    for K in range(args.Seed0, 51, args.Space):
+    for K in range(Seed_range[0], Seed_range[1], Seed_range[2]):
         ts = time.time()
         Mc_Inf = MC_RUN(net_path, Seed_Prepar[:K], mc=1000, device=device_eva)
         te = time.time()
         print("{} : opt={:.1f} eval_time={:.1f}s".format(K, Mc_Inf, te-ts))
-        SeedSize_Inf[K] = Mc_Inf
+        SeedSize_Inf[K] = [Seed_Prepar[:K], Mc_Inf]
     print("")
 
     return SeedSize_Inf
@@ -53,36 +52,39 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--Data_name", type=str, help="graph's name")
     parser.add_argument("--Lr", type=float, help="learning rate")
-    parser.add_argument("--Space", type=int, help="step length of seed set size")
     parser.add_argument("--Iter", type=int, help="maximum iteration times of optim")
     parser.add_argument("--Max_Iter", type=int, help="maximum iteration times of DMP")
     parser.add_argument("--OverLap", type=int,  help="OverLap order list")
     parser.add_argument("--OverLapRate", type=float,  help="OverLap rate list")
-    parser.add_argument("--Seed0", type=int,  default=1, help="the smallest seed size")
+    parser.add_argument("--Seed_range", type=int,  nargs="+")
+    parser.add_argument("--Device_opt", type=int,  default=-1, help="-1 for cpu")
+    parser.add_argument("--Device_eva", type=int,  default=2, help="-1 for cpu")
     args = parser.parse_args()
 
     Data_name = args.Data_name
     Lr = args.Lr
-    Space = args.Space
     Iter = args.Iter
     Max_Iter = args.Max_Iter
     OverLap = args.OverLap
     OverLapRate = args.OverLapRate
-    Seed0 = args.Seed0
+    Seed_range = args.Seed_range
+    range_str = "_".join([str(x) for x in Seed_range])
     
-    uniq_log = str(Data_name)+"_"+"_".join([str(par) for par in [Lr, Iter, Max_Iter, OverLap, OverLapRate]])
+    uniq_log = str(Data_name)+"_"+"_".join([str(par) for par in [Lr, Iter, Max_Iter, OverLap, OverLapRate]])+"_"+range_str
     
     net_path = "../data/test_graph/{}.npy".format(Data_name)
     if not os.path.exists("../results/{}".format(Data_name)):
         os.mkdir("../results/{}".format(Data_name))
     
-    save_path = "../results/{}/Once_{}.pkl".format(Data_name, uniq_log)
+    save_path = "../results/{}/{}.pkl".format(Data_name, uniq_log)
 
+    #device_opt = T.device("cpu") if args.Device_opt==-1 else T.device("cuda:{}".format(args.Device_opt))
     device_opt = T.device("cpu")
-    device_eva = T.device("cuda:0")
+    device_eva = T.device("cuda:{}".format(args.Device_eva))
+
     print("Preparing DMP module for {} .. ".format(Data_name))
     ts = time.time()
-    IC = DMP_IC(net_path, device_opt, Max_Iter)
+    IC = DMP_IC(net_path, T.device("cpu"), Max_Iter)
     G = IC.G
     print("Done, cost {:.1f}s".format(time.time()-ts))
 
--- codes/once.sh
+++ codes/once.sh
@@ -1,7 +1,34 @@
 #python main_once.py --Data_name NetHEPT --Lr 1E-4 --Space 1 --Iter 20 --Max_Iter 4 --OverLap 2 --OverLapRate 0.5 --Seed0 1
-#python main_once.py --Data_name Twitter --Lr 1E-4 --Space 2 --Iter 20 --Max_Iter 4 --OverLap 0 --OverLapRate 0 --Seed0 1
-#python main_once.py --Data_name Pokec --Lr 1E-6 --Space 5 --Iter 20 --Max_Iter 4 --OverLap 2 --OverLapRate 0.5 --Seed0 1
+#python main_once.py --Data_name Twitter --Lr 1E-4 --Space 2 --Iter 20 --Max_Iter 4 --OverLap 0 --OverLapRate 0 --Seed0 1n main_once.py --Data_name Pokec --Lr 1E-6 --Space 5 --Iter 20 --Max_Iter 4 --OverLap 2 --OverLapRate 0.5 --Seed0 1
 #python main_once.py --Data_name Pokec --Lr 1E-6 --Space 4 --Iter 30 --Max_Iter 4 --OverLap 1 --OverLapRate 0.5 --Seed0 1
-#python main_once.py --Data_name Pokec --Lr 1E-6 --Space 4 --Iter 30 --Max_Iter 4 --OverLap 0 --OverLapRate 0 --Seed0 1
-
-python main_once.py --Data_name LiveJournal1 --Lr 1E-6 --Space 4 --Iter 20 --Max_Iter 4 --OverLap 0 --OverLapRate 0 --Seed0 1
+#python main_once.py --Data_name Pokec --Lr 1E-6  --Iter 30 --Max_Iter 4 --OverLap 0 --OverLapRate 0 --Seed_range 1 52 5 --Device_opt 2 --Device_opt 2
+#python main_once.py --Data_name LiveJournal1 --Lr 1E-6  --Iter 30 --Max_Iter 4 --OverLap 0 --OverLapRate 0 --Seed_range 1 52 5 --Device_opt -1 --Device_opt 3
+#python main_once.py --Data_name Twitter --Lr 1E-4  --Iter 30 --Max_Iter 4 --OverLap 1 --OverLapRate 0.2 --Seed_range 1 52 4 --Device_opt 3 --Device_opt 3
+#python main_once.py --Data_name Twitter --Lr 5E-5  --Iter 50 --Max_Iter 4 --OverLap 1 --OverLapRate 0.2 --Seed_range 1 52 4 --Device_opt 3 --Device_opt 3
+#python main_once.py --Data_name Twitter --Lr 5E-5  --Iter 80 --Max_Iter 4 --OverLap 1 --OverLapRate 0.2 --Seed_range 1 52 4 --Device_opt 3 --Device_opt 3
+#python main_once.py --Data_name Twitter --Lr 5E-5  --Iter 80 --Max_Iter 4 --OverLap 0 --OverLapRate 0 --Seed_range 1 52 4 --Device_opt 3 --Device_opt 3
+#python main_once.py --Data_name Twitter --Lr 1E-4  --Iter 50 --Max_Iter 4 --OverLap 0 --OverLapRate 0 --Seed_range 1 52 4 --Device_opt 3 --Device_opt 3
+#python main_once.py --Data_name Twitter --Lr 1E-4  --Iter 80 --Max_Iter 4 --OverLap 0 --OverLapRate 0 --Seed_range 1 52 4 --Device_opt 3 --Device_opt 3
+#python main_once.py --Data_name Twitter --Lr 1E-4  --Iter 80 --Max_Iter 8 --OverLap 0 --OverLapRate 0 --Seed_range 1 52 4 --Device_opt 3 --Device_opt 3
+#python main_once.py --Data_name Twitter --Lr 1E-4  --Iter 80 --Max_Iter 12 --OverLap 0 --OverLapRate 0 --Seed_range 1 52 4 --Device_opt 3 --Device_opt 3
+#python main_once.py --Data_name Twitter --Lr 1E-4  --Iter 80 --Max_Iter 16 --OverLap 0 --OverLapRate 0 --Seed_range 1 52 4 --Device_opt 2 --Device_opt 2
+#python main_once.py --Data_name Twitter --Lr 1E-4  --Iter 80 --Max_Iter 6 --OverLap 0 --OverLapRate 0 --Seed_range 1 52 4 --Device_opt 4 --Device_opt 4
+#python main_once.py --Data_name Twitter --Lr 1E-4  --Iter 80 --Max_Iter 10 --OverLap 0 --OverLapRate 0 --Seed_range 1 52 4 --Device_opt 2 --Device_opt 2
+#python main_once.py --Data_name Twitter --Lr 1E-4  --Iter 20 --Max_Iter 8 --OverLap 0 --OverLapRate 0 --Seed_range 1 52 4 --Device_opt 2 --Device_opt 2
+#python main_once.py --Data_name Twitter --Lr 1E-4  --Iter 100 --Max_Iter 8 --OverLap 0 --OverLapRate 0 --Seed_range 1 52 4 --Device_opt 2 --Device_opt 2
+#python main_once.py --Data_name Twitter --Lr 1E-4  --Iter 200 --Max_Iter 8 --OverLap 0 --OverLapRate 0 --Seed_range 1 52 4 --Device_opt 2 --Device_opt 2
+#python main_once.py --Data_name NetHEPT --Lr 1E-4  --Iter 200 --Max_Iter 8 --OverLap 2 --OverLapRate 0.5 --Seed_range 1 52 4 --Device_opt 2 --Device_opt 2
+#python main_once.py --Data_name Twitter --Lr 2E-5  --Iter 200 --Max_Iter 8 --OverLap 0 --OverLapRate 0 --Seed_range 1 52 4 --Device_opt 2 --Device_opt 2
+#python main_once.py --Data_name Twitter --Lr 2E-5  --Iter 200 --Max_Iter 8 --OverLap 0 --OverLapRate 0 --Seed_range 1 52 4 --Device_opt 2 --Device_opt 2
+#python main_lbfgs.py --Data_name NetHEPT --Lr 1E-4  --Iter 101 --Max_Iter 8 --OverLap 2 --OverLapRate 0.5 --Seed_range 1 52 4 --Device_opt 2 --Device_opt 2
+#python main_once.py --Data_name NetHEPT --Lr 1E-4  --Iter 101 --Max_Iter 8 --OverLap 2 --OverLapRate 0.5 --Seed_range 1 52 4 --Device_opt 2 --Device_opt 2
+#python main_once.py --Data_name Twitter --Lr 1E-4  --Iter 200 --Max_Iter 8 --OverLap 0 --OverLapRate 0 --Seed_range 1 52 4 --Device_opt 2 --Device_opt 2
+#python main_once.py --Data_name Twitter --Lr 1E-4  --Iter 400 --Max_Iter 8 --OverLap 0 --OverLapRate 0 --Seed_range 1 52 4 --Device_opt 2 --Device_opt 2
+#python main_once.py --Data_name Twitter --Lr 1E-4  --Iter 100 --Max_Iter 8 --OverLap 0 --OverLapRate 0 --Seed_range 1 52 4 --Device_opt 2 --Device_opt 2
+#python main_once.py --Data_name Pokec --Lr 1E-6  --Iter 100 --Max_Iter 8 --OverLap 0 --OverLapRate 0 --Seed_range 1 52 5 --Device_opt 2 --Device_opt 2
+#python main_once.py --Data_name LiveJournal1 --Lr 1E-6  --Iter 100 --Max_Iter 8 --OverLap 0 --OverLapRate 0 --Seed_range 1 52 5 --Device_opt -1 --Device_opt 3
+#python main_once.py --Data_name Twitter --Lr 1E-4  --Iter 600 --Max_Iter 8 --OverLap 0 --OverLapRate 0 --Seed_range 1 52 4 --Device_opt 4 --Device_opt 4
+#python main_once.py --Data_name Pokec --Lr 1E-6  --Iter 300 --Max_Iter 8 --OverLap 0 --OverLapRate 0 --Seed_range 1 52 5 --Device_opt 2 --Device_opt 2
+#python main_once.py --Data_name Twitter --Lr 1E-4  --Iter 600 --Max_Iter 8 --OverLap 0 --OverLapRate 0 --Seed_range 1 52 4 --Device_opt 1 --Device_opt 1
+#python main_once.py --Data_name Twitter --Lr 1E-4  --Iter 300 --Max_Iter 8 --OverLap 0 --OverLapRate 0 --Seed_range 1 52 4 --Device_opt 1 --Device_opt 1
+python main_once.py --Data_name LiveJournal1 --Lr 1E-6  --Iter 300 --Max_Iter 8 --OverLap 0 --OverLapRate 0 --Seed_range 1 52 5 --Device_opt -1 --Device_opt 3
